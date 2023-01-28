ddabe2bdde86eddf5b12b7844e70567c0b40962e
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-01-28 13:59:08 +0100

    Get-GroupMembers.ps1
    Get members of a specific group using graph api.

---
+++ Get-GroupMembers.ps1
@@ -0,0 +1,11 @@
+# Get members of a specific group
+function Get-GroupMembers
+{
+param ($MCCgroup)
+$graphApiVersion = "beta/"
+$Resource = ("groups`?`$filter=displayName eq $($MCCgroup)")
+$uri = 'https://graph.microsoft.com/'
+$MCCgroupsResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method Get -Verbose)
+$MCCgroups = $MCCgroupsResponse.value
+return $MCCgroups
+}
