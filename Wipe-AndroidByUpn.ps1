31bd14a5ac13ae52d84c46ec6cb02da8db2f3043
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-02-05 18:23:04 +0100

    Update Wipe-AndroidByUpn.ps1

--- Wipe-AndroidByUpn.ps1
+++ Wipe-AndroidByUpn.ps1
@@ -4,15 +4,5 @@ param($Userprincipalname)
 $graphApiVersion = "beta/"
 $Resource = ("deviceManagement/managedDevices`?`$filter=userprincipalname eq '$Userprincipalname' and operatingSystem eq 'android'")
 $uri = 'https://graph.microsoft.com/'
-$MCCdeviceResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method Get -Verbose)
-    $MCCdevice = $MCCdeviceResponse.value
-    $data = @'
-{
-  "keepEnrollmentData": false,
-  "keepUserData": false
-}
-'@
-    $graphApiVersion = "beta/"
-    $Resource = ("deviceManagement/managedDevices/$(($MCCdevice).Id)/wipe")
-    (Invoke-RestMethod -Uri ("$($uri)$($graphApiVersion)$($Resource)`n$($data)") -ContentType application/json  –Headers $authToken –Method POST -Verbose)
+(Invoke-RestMethod -Uri ("$($uri)$($graphApiVersion)$($Resource)") –Headers $authToken –Method POST -Body $data -ContentType "application/json" -Verbose)  
 }
