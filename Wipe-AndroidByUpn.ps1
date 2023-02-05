a0b451ea488e7c05e8e3c54abf73bffb5bf53ac5
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-02-05 18:03:53 +0100

    Wipe-AndroidByUpn.ps1

---
+++ Wipe-AndroidByUpn.ps1
@@ -0,0 +1,18 @@
+function Wipe-AndroidByUpn
+{
+param($Userprincipalname)
+$graphApiVersion = "beta/"
+$Resource = ("deviceManagement/managedDevices`?`$filter=userprincipalname eq '$MCCupn' and operatingSystem eq 'android'")
+$uri = 'https://graph.microsoft.com/'
+$MCCdeviceResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method Get -Verbose)
+    $MCCdevice = $MCCdeviceResponse.value
+    $data = @'
+{
+  "keepEnrollmentData": false,
+  "keepUserData": false
+}
+'@
+    $graphApiVersion = "beta/"
+    $Resource = ("deviceManagement/managedDevices/$(($MCCdevice).Id)/wipe")
+    (Invoke-RestMethod -Uri ("$($uri)$($graphApiVersion)$($Resource)`n$($data)") -ContentType application/json  –Headers $authToken –Method POST -Verbose)
+}
