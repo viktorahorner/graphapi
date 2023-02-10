b94ed6af3d0cb2768d67877a958e6446fcebc18c
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-02-10 13:32:23 +0100

    Update Get-MobileAppConfigurations.ps1

--- Get-MobileAppConfigurations.ps1
+++ Get-MobileAppConfigurations.ps1
@@ -3,6 +3,7 @@ function Get-MobileAppConfigurations
 $graphApiVersion = "Beta"
 $Resource = "/deviceAppManagement/mobileAppConfigurations"
 $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-$MCCmanageddevices = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get)
-$MCCmanageddevices.value | select packageId, displayname
+$MCCAppConfigurations = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get)
+$MCCAppConfigurations.value | select packageId, displayname
+return $MCCAppConfigurations.value
 }
