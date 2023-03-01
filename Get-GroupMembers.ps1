9c48ecac3a5609a5180f03282dad5871df306809
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-03-01 15:02:16 +0100

    Update Get-GroupMembers.ps1

--- Get-GroupMembers.ps1
+++ Get-GroupMembers.ps1
@@ -3,7 +3,7 @@ function Get-GroupMembers
 {
 param ($MCCgroup)
 $graphApiVersion = "beta/"
-$Resource = ("groups`?`$filter=displayName eq '$($MCCgroup)'")
+$Resource = ("groups`?`$filter=displayName eq '$MCCgroup'")
 $uri = 'https://graph.microsoft.com/'
 $MCCgroupsResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method Get -Verbose)
 $MCCgroups = $MCCgroupsResponse.value
