fe74b3ad26ed8c6f6764083da0ec0634c3a09e37
viktorahorner <48084834+viktorahorner@users.noreply.github.com>
2023-03-19 10:38:30 +0100

    Create Add-GroupMember.ps1

---
+++ Add-GroupMember.ps1
@@ -0,0 +1,30 @@
+function Get-GroupByDisplayname
+{
+param ($displayname)
+$graphApiVersion = "beta/"
+$Resource = ("groups`?`$filter=displayName eq '$displayname'")
+$uri = 'https://graph.microsoft.com/'
+$MCCgroupsResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method Get -Verbose)
+return $MCCgroupsResponse.value
+}
+function Add-GroupMember
+{
+param($GroupDisplayname, $UserPrincipalname)
+$graphApiVersion = "v1.0/"
+$MCCuser = $UserPrincipalname
+$Resource = ("users/")
+$uri = 'https://graph.microsoft.com/'
+$MCCusersResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource+$MCCuser) –Headers $authToken –Method Get -Verbose)
+$MCCusersResponse.id
+
+$params = @"
+{
+    "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/$($MCCusersResponse.id)"
+}
+"@
+$group = Get-GroupByDisplayname -displayname $GroupDisplayname
+$graphApiVersion = "beta/"
+$Resource = ("groups/`{"+$group.Id+"`}/members/`$ref")
+$uri = 'https://graph.microsoft.com/'
+$MCCgroupsResponse = (Invoke-RestMethod -Uri ($uri+$graphApiVersion+$Resource) –Headers $authToken –Method POST -Body $params -Verbose -ContentType application/json)
+}
